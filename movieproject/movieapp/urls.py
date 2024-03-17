7dd3b0de0a8a3cdc8e7b157a9d8be1d192621081
snehak2220 <snehaammuz116@gmail.com>
2024-03-17 21:25:26 +0530

    added

---
+++ movieproject/movieapp/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from . import views
+app_name='movieapp'
+
+urlpatterns = [
+
+    path('',views.index,name='index'),
+    path('movie/<int:movie_id>/',views.detail,name='detail'),
+    path('add/',views.add,name='add_movie'),
+    path('update/<int:id>/',views.update,name='update'),
+    path('delete/<int:id>/',views.delete,name='delete')
+]

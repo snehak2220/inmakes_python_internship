c76f27f58ae9cbc325fcaea058d1d5211d57a5a9
Sneha k <74949282+snehak2220@users.noreply.github.com>
2024-04-09 12:22:09 +0530

    Delete Ecommerce/EcommerceProject directory

--- Ecommerce/EcommerceProject/EcommerceApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EcommerceappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'EcommerceApp'
--- Ecommerce/EcommerceProject/EcommerceApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-app_name='EcommerceApp'
-urlpatterns = [
-    path('',views.allProdCat,name='allProdCat'),
-    path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
-    path('<slug:c_slug>/<slug:product_slug>/',views.proDetails,name='prodCatDetail'),
-]
